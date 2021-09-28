baaeed1c14e6156d7fb081b4e80dbbe62ae1e322
Phantomat0 <soldierxfreedom@gmail.com>
2021-09-28 09:19:02 -0500

    oAuth authentunication with google services

--- index.js
+++ index.js
@@ -1,25 +1,106 @@
-require('dotenv').config();
-const Discord = require('discord.js');
-const bot = new Discord.Client();
-const TOKEN = process.env.TOKEN;
+const fs = require("fs");
+const readline = require("readline");
+const { google } = require("googleapis");
 
-bot.login(TOKEN);
+// If modifying these scopes, delete token.json.
+const SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"];
+// The file token.json stores the user's access and refresh tokens, and is
+// created automatically when the authorization flow completes for the first
+// time.
+const TOKEN_PATH = "token.json";
 
-bot.on('ready', () => {
-  console.info(`Logged in as ${bot.user.tag}!`);
+let oAuth2Client = null;
+
+// Load client secrets from a local file.
+fs.readFile("credentials.json", (err, content) => {
+  if (err) return console.log("Error loading client secret file:", err);
+  // Authorize a client with credentials, then call the Google Sheets API.
+  authorize(JSON.parse(content), listMajors);
 });
 
-bot.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('pong');
-    msg.channel.send('pong');
+/**
+ * Create an OAuth2 client with the given credentials, and then execute the
+ * given callback function.
+ * @param {Object} credentials The authorization client credentials.
+ * @param {function} callback The callback to call with the authorized client.
+ */
 
-  } else if (msg.content.startsWith('!kick')) {
-    if (msg.mentions.users.size) {
-      const taggedUser = msg.mentions.users.first();
-      msg.channel.send(`You wanted to kick: ${taggedUser.username}`);
-    } else {
-      msg.reply('Please tag a valid user!');
+function authorize(credentials) {
+  const { client_secret, client_id, redirect_uris } = credentials.installed;
+  oAuth2Client = new google.auth.OAuth2(
+    client_id,
+    client_secret,
+    redirect_uris[0]
+  );
+
+  // Check if we have previously stored a token.
+  fs.readFile(TOKEN_PATH, (err, token) => {
+    if (err) return getNewToken(oAuth2Client, callback);
+    oAuth2Client.setCredentials(JSON.parse(token));
+  });
+}
+
+/**
+ * Get and store new token after prompting for user authorization, and then
+ * execute the given callback with the authorized OAuth2 client.
+ * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
+ * @param {getEventsCallback} callback The callback for the authorized client.
+ */
+function getNewToken(oAuth2Client, callback) {
+  const authUrl = oAuth2Client.generateAuthUrl({
+    access_type: "offline",
+    scope: SCOPES,
+  });
+  console.log("Authorize this app by visiting this url:", authUrl);
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  rl.question("Enter the code from that page here: ", (code) => {
+    rl.close();
+    oAuth2Client.getToken(code, (err, token) => {
+      if (err)
+        return console.error(
+          "Error while trying to retrieve access token",
+          err
+        );
+      oAuth2Client.setCredentials(token);
+      // Store the token to disk for later program executions
+      fs.writeFile(TOKEN_PATH, JSON.stringify(token), (err) => {
+        if (err) return console.error(err);
+        console.log("Token stored to", TOKEN_PATH);
+      });
+      callback(oAuth2Client);
+    });
+  });
+}
+
+class Sheet {
+  constructor(params) {}
+}
+
+function listMajors() {
+  const sheets = google.sheets({ version: "v4", auth: oAuth2Client });
+  sheets.spreadsheets.values.get(
+    {
+      spreadsheetId: "1Er2liiHoO6Q9WQxUWSyJ_jOuKmjwMaT-45RtI0MAiDU",
+      range: "Sheet1!A1:B4",
+    },
+    (err, res) => {
+      if (err) return console.log("The API returned an error: " + err);
+      const rows = res.data.values;
+      if (rows.length) {
+        console.log(rows);
+        console.log("hey lol");
+        // Print columns A and E, which correspond to indices 0 and 4.
+        // rows.map((row) => {
+        //   console.log(`${row[0]}, ${row[4]}`);
+        // });
+      } else {
+        console.log("No data found.");
+      }
     }
-  }
-});
+  );
+}
+
+module.exports = { listMajors };
