6671540b3b16be8ee7ba1880463b4aebb67ad1aa
Phantomat0 <soldierxfreedom@gmail.com>
2021-09-28 21:14:21 -0500

    Added discord and sheet folder

--- sheets/sheet.js
+++ sheets/sheet.js
@@ -1,21 +1,21 @@
 const fs = require("fs");
 const readline = require("readline");
 const { google } = require("googleapis");
+const { master_sheet_id } = require("./config.json");
 
 // If modifying these scopes, delete token.json.
-const SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"];
-// The file token.json stores the user's access and refresh tokens, and is
-// created automatically when the authorization flow completes for the first
-// time.
-const TOKEN_PATH = "token.json";
+const SCOPES = ["https://www.googleapis.com/auth/spreadsheets"];
+const TOKEN_PATH = "sheets/token.json";
 
 let oAuth2Client = null;
 
+// Sheet initialization
+
 // Load client secrets from a local file.
-fs.readFile("credentials.json", (err, content) => {
+fs.readFile("sheets/credentials.json", (err, content) => {
   if (err) return console.log("Error loading client secret file:", err);
   // Authorize a client with credentials, then call the Google Sheets API.
-  authorize(JSON.parse(content), listMajors);
+  authorize(JSON.parse(content));
 });
 
 /**
@@ -35,7 +35,7 @@ function authorize(credentials) {
 
   // Check if we have previously stored a token.
   fs.readFile(TOKEN_PATH, (err, token) => {
-    if (err) return getNewToken(oAuth2Client, callback);
+    if (err) return getNewToken(oAuth2Client);
     oAuth2Client.setCredentials(JSON.parse(token));
   });
 }
@@ -46,7 +46,7 @@ function authorize(credentials) {
  * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
  * @param {getEventsCallback} callback The callback for the authorized client.
  */
-function getNewToken(oAuth2Client, callback) {
+function getNewToken(oAuth2Client) {
   const authUrl = oAuth2Client.generateAuthUrl({
     access_type: "offline",
     scope: SCOPES,
@@ -70,37 +70,51 @@ function getNewToken(oAuth2Client, callback) {
         if (err) return console.error(err);
         console.log("Token stored to", TOKEN_PATH);
       });
-      callback(oAuth2Client);
     });
   });
 }
 
-class Sheet {
-  constructor(params) {}
-}
+// Sheet Obj
 
-function listMajors() {
-  const sheets = google.sheets({ version: "v4", auth: oAuth2Client });
-  sheets.spreadsheets.values.get(
-    {
-      spreadsheetId: "1Er2liiHoO6Q9WQxUWSyJ_jOuKmjwMaT-45RtI0MAiDU",
-      range: "Sheet1!A1:B4",
-    },
-    (err, res) => {
-      if (err) return console.log("The API returned an error: " + err);
-      const rows = res.data.values;
-      if (rows.length) {
-        console.log(rows);
-        console.log("hey lol");
-        // Print columns A and E, which correspond to indices 0 and 4.
-        // rows.map((row) => {
-        //   console.log(`${row[0]}, ${row[4]}`);
-        // });
-      } else {
-        console.log("No data found.");
+const Sheet = {
+  getSheet() {
+    return google.sheets({ version: "v4", auth: oAuth2Client });
+  },
+
+  list() {
+    this.getSheet().spreadsheets.values.get(
+      {
+        spreadsheetId: master_sheet_id,
+        range: "Sheet1!A1:B4",
+      },
+
+      (err, res) => {
+        if (err) return console.log("The API returned an error: " + err);
+        const rows = res.data.values;
+        if (rows.length) {
+          console.log(rows);
+          console.log("hey LIST WORKED");
+          // Print columns A and E, which correspond to indices 0 and 4.
+          // rows.map((row) => {
+          //   console.log(`${row[0]}, ${row[4]}`);
+          // });
+        } else {
+          console.log("No data found.");
+        }
       }
-    }
-  );
-}
+    );
+  },
 
-module.exports = { listMajors };
+  write(data) {
+    this.getSheet().spreadsheets.values.append({
+      spreadsheetId: SHEET_ID,
+      range: "Sheet1!F:G",
+      valueInputOption: "USER_ENTERED",
+      resource: {
+        values: [["hey lol", "whats goodie!"]],
+      },
+    });
+  },
+};
+
+module.exports = Sheet;
