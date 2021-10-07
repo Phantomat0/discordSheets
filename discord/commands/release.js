80a59b84b4eddb2ea6d27e6f08db8a57898bad6e
Phantomat0 <soldierxfreedom@gmail.com>
2021-10-07 16:58:06 -0500

    Handled command errors

--- discord/commands/release.js
+++ discord/commands/release.js
@@ -1,5 +1,4 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const { RedTriangle, Warning } = require("../icons");
 const mainDatabase = require("../../database/main/main");
 const { MY_GM_ID } = require("../config/channels");
 const {
@@ -7,6 +6,7 @@ const {
   MessageActionRow,
   MessageSelectMenu,
 } = require("discord.js");
+const { CommandError, InvalidPermissionError } = require("../bot-util");
 
 module.exports = {
   perms: ["Admin", "General Manager"],
@@ -23,169 +23,113 @@ module.exports = {
     ),
 
   async execute(interaction) {
-    try {
-      if (interaction.channelId !== MY_GM_ID)
-        throw {
-          type: "Invalid Channel",
-          message: `This command is only available in <#${MY_GM_ID}>`,
-        };
+    if (interaction.channelId !== MY_GM_ID)
+      throw new CommandError(
+        "Invalid Channel",
+        `This command is only available in <#${MY_GM_ID}>`
+      );
 
-      if (this.perms.length !== 0) {
-        const hasPerms = interaction.member.roles.cache.some((role) =>
-          this.perms.includes(role.name)
+    if (this.perms.length !== 0) {
+      const hasPerms = interaction.member.roles.cache.some((role) =>
+        this.perms.includes(role.name)
+      );
+
+      if (!hasPerms)
+        throw new InvalidPermissionError(
+          `You are not authorized to use that command!`
         );
 
-        if (!hasPerms)
-          throw {
-            type: "Invalid Permissions",
-            message: `You are not authorized to use that command!`,
-          };
+      const playerProfile = await mainDatabase.getPlayerByDiscordID(
+        interaction.user.id
+      );
 
-        // We have to get the players team_id by discord
-        // Get the players teams
-        // Get either D1 team or D2, depending on the selection
-
-        const playerProfile = await mainDatabase.getPlayerByDiscordID(
-          interaction.user.id
+      if (playerProfile === null)
+        throw new InvalidPermissionError(
+          `You are not registered as a player in the database`
         );
 
-        if (playerProfile === null)
-          throw {
-            type: "Invalid Permissions",
-            message: `You are not registered as a player in the database`,
-          };
+      const { player_id } = playerProfile;
 
-        const { player_id } = playerProfile;
+      const teamProfile = await mainDatabase.getManagersTeam(player_id);
 
-        const teamProfile = await mainDatabase.getManagersTeam(player_id);
+      if (teamProfile === null)
+        throw new InvalidPermissionError(
+          `That team does not exist or you are not the General Manager of it`
+        );
 
-        if (teamProfile === null)
-          throw {
-            type: "Invalid Permissions",
-            message: `That team does not exist or you are not the General Manager of it`,
-          };
+      const divisionOption = interaction.options.getString("division");
 
-        const divisionOption = interaction.options.getString("division");
+      let teamPlayers = [];
 
-        let teamPlayers = [];
+      const { team_id, division_id } = teamProfile;
 
-        const { team_id, division_id } = teamProfile;
+      if (divisionOption === "division_1") {
+        // D2 GM trying to access D1 roster
+        if (division_id == 2)
+          throw new InvalidPermissionError(
+            `You are not a manager of a D1 Team`
+          );
+        teamPlayers = await mainDatabase.getPlayersByTeam(team_id);
+      }
 
-        console.log(divisionOption);
-        console.log(teamProfile);
+      if (divisionOption === "division_2") {
+        // Is this guy the manager of a D1 team?
 
-        if (divisionOption === "division_1") {
-          console.log(division_id, "divID");
-          // D2 GM trying to access D1 roster
-          if (division_id == 2)
-            throw {
-              type: "Invalid Permissions",
-              message: `You are not a manager of a D1 Team`,
-            };
-          console.log(team_id);
+        if (division_id == 1) {
+          // Show his D2 Team
+          const affiliateTeam = await mainDatabase.getTeamsAffiliate(team_id);
+
+          teamPlayers = await mainDatabase.getPlayersByTeam(
+            affiliateTeam.team_id
+          );
+        } else {
           teamPlayers = await mainDatabase.getPlayersByTeam(team_id);
         }
-
-        if (divisionOption === "division_2") {
-          // Is this guy the manager of a D1 team?
-
-          console.log(division_id, "divid");
-
-          if (division_id == 1) {
-            // Show his D2 Team
-            const affiliateTeam = await mainDatabase.getTeamsAffiliate(team_id);
-
-            console.log(affiliateTeam);
-
-            teamPlayers = await mainDatabase.getPlayersByTeam(
-              affiliateTeam.team_id
-            );
-          } else {
-            teamPlayers = await mainDatabase.getPlayersByTeam(team_id);
-          }
-        }
-
-        // Find the team that this player manages
-
-        // If he manages more than one team, throw an error
-
-        // Once we find the team that he manages, check what option he selected
-
-        // If d1, show his d1 team
-        // If d2, show the players on his d2 team
-
-        // Now we have to check if they selected D1 or D2
-
-        // If the players main team is D2, we can skip this, since its automatic D2
-
-        if (teamPlayers.length === 0)
-          throw {
-            type: "No players",
-            message: `There are no active players on that team!`,
-          };
-
-        const messageSelect = new MessageSelectMenu()
-          .setCustomId("playerSelectRelease")
-          .setPlaceholder("No player selected")
-          .setMinValues(1)
-          .setMaxValues(1);
-
-        // Now lets map that to the messageSelect options format
-        const playerOptionsArray = teamPlayers.map((player) => {
-          return {
-            label: player.player_name,
-            value: player.player_id,
-          };
-        });
-
-        messageSelect.addOptions(playerOptionsArray);
-
-        const playerSelectMenu = new MessageActionRow().addComponents(
-          messageSelect
-        );
-
-        const selectEmbed = new MessageEmbed().setTitle(
-          "Select a player to release"
-        );
-
-        await interaction.reply({
-          embeds: [selectEmbed],
-          components: [playerSelectMenu],
-          ephemeral: true,
-        });
-
-        setTimeout(() => {
-          const timeOutEmbed = new MessageEmbed().setTitle("Command timed out");
-          interaction.editReply({
-            embeds: [timeOutEmbed],
-            components: [],
-          });
-        }, 15000);
-      }
-    } catch (error) {
-      if (error?.type) {
-        const commandErrorEmbed = new MessageEmbed()
-          .setColor("#FF5733")
-          .setTitle(`${RedTriangle} Command Error`)
-          .setDescription(`${error.type}: ${error.message}`);
-
-        interaction.reply({
-          embeds: [commandErrorEmbed],
-          ephemeral: true,
-        });
-      } else {
-        console.log(error);
-        const errorEmbed = new MessageEmbed().setTitle(
-          `${Warning} There was an error`
-        );
-
-        interaction.reply({
-          embeds: [errorEmbed],
-          ephemeral: true,
-        });
       }
 
-      return;
+      if (teamPlayers.length === 0)
+        throw new CommandError(
+          "No players",
+          `There are no active players on that team!`
+        );
+
+      const messageSelect = new MessageSelectMenu()
+        .setCustomId("playerSelectRelease")
+        .setPlaceholder("No player selected")
+        .setMinValues(1)
+        .setMaxValues(1);
+
+      // Now lets map that to the messageSelect options format
+      const playerOptionsArray = teamPlayers.map((player) => {
+        return {
+          label: player.player_name,
+          value: player.player_id,
+        };
+      });
+
+      messageSelect.addOptions(playerOptionsArray);
+
+      const playerSelectMenu = new MessageActionRow().addComponents(
+        messageSelect
+      );
+
+      const selectEmbed = new MessageEmbed().setTitle(
+        "Select a player to release"
+      );
+
+      await interaction.reply({
+        embeds: [selectEmbed],
+        components: [playerSelectMenu],
+        ephemeral: true,
+      });
+
+      setTimeout(() => {
+        const timeOutEmbed = new MessageEmbed().setTitle("Command timed out");
+        interaction.editReply({
+          embeds: [timeOutEmbed],
+          components: [],
+        });
+      }, 15000);
     }
   },
 };
--- discord/events/interactionCreate.js
+++ discord/events/interactionCreate.js
@@ -3,8 +3,10 @@ const {
   MessageSelectMenu,
   MessageActionRow,
   MessageButton,
+  Interaction,
 } = require("discord.js");
-const { FREE_AGENT_ROLE_ID } = require("../config/roles.json");
+const { FREE_AGENT_ROLE_ID } = require("../config/roles");
+const { TRANSACTIONS_ID } = require("../config/channels");
 const {
   QuestionMark,
   PageCurled,
@@ -13,57 +15,90 @@ const {
   OkHand,
   X,
   GreenCheck,
+  Warning,
 } = require("../icons");
 const mainDatabase = require("../../database/main/main");
 
 const interactionManager = {
   async handleButton(interaction) {
-    interaction.message.embeds[0].fields;
-    console.log(interaction);
+    const { customId } = interaction;
 
-    const embedFields = interaction.message.embeds[0].fields;
-    console.log(embedFields);
-    const idField = embedFields.find((field) => {
-      console.log(field);
-      return field.name === "player_id";
-    });
+    if (customId === "releaseConfirm") {
+      const embedFields = interaction.message.embeds[0].fields;
+      const idField = embedFields.find((field) => field.name === "player_id");
 
-    const { value: playerID } = idField;
+      const { value: playerID } = idField;
 
-    const playerProfile = await mainDatabase.getPlayerByID(playerID);
-    console.log(playerProfile);
-    const discordProfile = await mainDatabase.getDiscordByPlayerID(playerID);
-    console.log(discordProfile);
-    const teamProfile = await mainDatabase.getTeam(
-      playerProfile.current_team_id
-    );
-    console.log(teamProfile);
-
-    const player = interaction.guild.members.cache.get(
-      discordProfile.discord_id
-    );
-    player.roles.add(FREE_AGENT_ROLE_ID);
-    player.roles.remove(teamProfile.role_id);
-
-    const successEmbed = new MessageEmbed()
-      .setColor("#75FF33")
-      .setTitle(`${GreenCheck} Successful release`)
-      .setDescription(
-        `${playerProfile.player_name} has been released from the ${teamProfile.name} and is now a Free Agent.`
+      const playerProfile = await mainDatabase.getPlayerByID(playerID);
+      const discordProfile = await mainDatabase.getDiscordByPlayerID(playerID);
+      const teamProfile = await mainDatabase.getTeam(
+        playerProfile.current_team_id
       );
 
-    await interaction.update({
-      embeds: [successEmbed],
-      components: [],
-    });
+      const playerBeingReleasedDiscord =
+        interaction.guild.members.cache.get(discordProfile.discord_id) ?? null;
 
-    // Ok once its successful release we have to:
-    /*
-1. Remove the players roleID DONE
-2. Add the FA Role DONE
-3. Change him teamID to 0
+      playerBeingReleasedDiscord?.roles.add(FREE_AGENT_ROLE_ID);
+      playerBeingReleasedDiscord?.roles.remove(teamProfile.role_id);
 
-*/
+      // Change his team in the database, 0 is the no team role
+      mainDatabase.updatePlayerTeam(playerProfile.player_id, 0);
+
+      const successEmbed = new MessageEmbed()
+        .setColor("#75FF33")
+        .setTitle(`${GreenCheck} Successful release`)
+        .setDescription(
+          `${playerProfile.player_name} has been released from the **${teamProfile.name}** and is now a Free Agent.`
+        )
+        .setTimestamp();
+
+      const releaseEmbed = new MessageEmbed()
+        .setColor(teamProfile.color)
+        .setAuthor(
+          `The ${teamProfile.name} release ${playerProfile.player_name}`,
+          teamProfile.logo_url
+        )
+        .setDescription(`<@${discordProfile.discord_id}> is now a Free Agent.`)
+        .setTimestamp()
+        .setFooter(
+          `${interaction.user.username}`,
+          interaction.user.displayAvatarURL()
+        );
+
+      await interaction.client.channels.cache.get(TRANSACTIONS_ID).send({
+        embeds: [releaseEmbed],
+      });
+
+      if (playerBeingReleasedDiscord === null) {
+        successEmbed.addField(
+          "Note",
+          `${playerProfile.player_name} is no longer in the Discord`
+        );
+      }
+
+      // Now we just have to change his team in the database
+
+      const timeOutEmbed = new MessageEmbed().setTitle("Command Complete");
+
+      await interaction.update({
+        embeds: [timeOutEmbed],
+        components: [],
+      });
+
+      await interaction.followUp({
+        embeds: [successEmbed],
+        ephemeral: true,
+      });
+    }
+
+    if (customId === "releaseCancel") {
+      const cancelEmbed = new MessageEmbed().setTitle("Command Cancelled");
+
+      await interaction.update({
+        embeds: [cancelEmbed],
+        components: [],
+      });
+    }
   },
   async handleCommand(interaction) {
     const command = interaction.client.commands.get(interaction.commandName);
@@ -73,12 +108,22 @@ const interactionManager = {
     try {
       await command.execute(interaction);
     } catch (error) {
-      console.error(error);
+      const getErrorEmbed = () => {
+        if (error.type) {
+          return new MessageEmbed()
+            .setColor(error.color)
+            .setTitle(error.name)
+            .setDescription(error.getMessage());
+        } else {
+          console.log(error);
+          return new MessageEmbed()
+            .setColor("#C70039")
+            .setTitle(`${Warning} Command Error`)
+            .setDescription("There was an error while executing this command!");
+        }
+      };
 
-      const errorEmbed = new MessageEmbed()
-        .setColor("#C70039")
-        .setTitle("Command Error")
-        .setDescription("There was an error while executing this command!");
+      const errorEmbed = getErrorEmbed();
 
       await interaction.reply({
         embeds: [errorEmbed],
@@ -92,24 +137,20 @@ const interactionManager = {
     if (customId === "playerSelectRelease") {
       const { values } = interaction;
 
-      // await interaction.deferReply({ ephemeral: true });
-
       const player = await mainDatabase.getPlayerByID(values[0]);
       const discordProfile = await mainDatabase.getDiscordByPlayerID(values[0]);
-
-      console.log(player);
 
       const { discord_id, discord_avatar_url } = discordProfile;
       const { player_name, player_id, current_team_id } = player;
 
-      console.log(player_name);
+      const teamProfile = await mainDatabase.getTeam(current_team_id);
 
       const confirmButton = new MessageButton()
         .setCustomId("releaseConfirm")
         .setLabel("Confirm")
         .setStyle("SUCCESS");
       const cancelButton = new MessageButton()
-        .setCustomId("cancelConfirm")
+        .setCustomId("releaseCancel")
         .setLabel("Cancel")
         .setStyle("SECONDARY");
 
@@ -118,7 +159,6 @@ const interactionManager = {
         confirmButton
       );
 
-      // Make the confirm message be the color of the team?
       const embed = new MessageEmbed()
         .setColor("RANDOM")
         .setTitle(`Are you sure you want to release ${player_name}?`)
@@ -126,14 +166,8 @@ const interactionManager = {
         .setDescription(`<@${discord_id}>`)
         .addFields(
           { name: "player_id", value: `${player_id}`, inline: true },
-          { name: "team", value: `${current_team_id}`, inline: true }
-        )
-        .setTimestamp();
-
-      // await interaction.editReply({
-      //   embeds: [embed],
-      //   components: [buttons],
-      // });
+          { name: "team", value: `${teamProfile.name}`, inline: true }
+        );
 
       await interaction.update({
         embeds: [embed],
