21b32ed66516a511ccc63dbf680efc63fa45a021
Phantomat0 <soldierxfreedom@gmail.com>
2021-10-23 14:01:14 -0500

    Moves channel IDs to channels file

--- discord/events/messageCreate.js
+++ discord/events/messageCreate.js
@@ -1,14 +1,15 @@
+const { SUGGESTIONS_ID, REGISTRATION_ID } = require("../config/channels");
+const { OkHand, X } = require("../icons");
+
 module.exports = {
   name: "messageCreate",
   async execute(message) {
     if (message.author.bot) return;
-    const REGISTRATION_CHANNEL_ID = "892959251788615712";
-    const SUGGESTION_CHANNEL_ID = "888561683520430130";
-    if (message.channel.id === REGISTRATION_CHANNEL_ID) {
+    if (message.channel.id === REGISTRATION_ID) {
       await message.delete();
     }
 
-    if (message.channel.id === SUGGESTION_CHANNEL_ID) {
+    if (message.channel.id === SUGGESTIONS_ID) {
       message.react(OkHand);
       message.react(X);
     }
