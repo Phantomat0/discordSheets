e152bd5718ca15ef7a11c1abc09440f27165ea83
Phantomat0 <soldierxfreedom@gmail.com>
2021-10-23 13:59:19 -0500

    Moved out errors and removed discordAvatarURL and discordName

--- discord/commands/register.js
+++ discord/commands/register.js
@@ -2,7 +2,9 @@ const { SlashCommandBuilder } = require("@discordjs/builders");
 const { MessageEmbed } = require("discord.js");
 const mainDatabase = require("../../database/main/main");
 const { updateSignUpList } = require("../bot-util");
-const { GreenCheck, RedTriangle } = require("../icons");
+const { successEmbed } = require("../embeds");
+const { FREE_AGENT_ROLE_ID } = require("../config/roles");
+const { REGISTERED_ID } = require("../config/channels");
 
 const PLAYER_POSITIONS = [
   ["ST", "ST"],
@@ -13,6 +15,8 @@ const PLAYER_POSITIONS = [
 ];
 
 module.exports = {
+  allowedRoles: [],
+  allowedChannels: [],
   data: new SlashCommandBuilder()
     .setName("register")
     .setDescription("Register for the league")
@@ -43,45 +47,19 @@ module.exports = {
     .addStringOption((option) =>
       option.setName("info").setDescription("Additional Info")
     ),
-  async execute(interaction, bot) {
+  async execute(interaction) {
     const playerName = interaction.options.getString("username");
     const availability = interaction.options.getInteger("availability");
     const position = interaction.options.getString("position");
     const info = interaction.options.getString("info");
 
-    await interaction.deferReply({ ephemeral: true });
-
-    try {
-      await mainDatabase.registerSignUp({
-        discordID: interaction.user.id,
-        discordName: interaction.user.username,
-        discordAvatarURL: interaction.user.displayAvatarURL(),
-        playerName: playerName,
-        position: position,
-        availability: availability,
-        info: info,
-      });
-    } catch (error) {
-      console.log(error);
-      if (error?.type === "Registration Error") {
-        const errorEmbed = new MessageEmbed()
-          .setColor("#FF5733")
-          .setTitle(`${RedTriangle} Registration Error`)
-          .setDescription(`${error.message}`);
-
-        await interaction.editReply({
-          embeds: [errorEmbed],
-        });
-        return;
-      }
-    }
-
-    const successEmbed = new MessageEmbed()
-      .setColor("#75FF33")
-      .setTitle(`${GreenCheck} You are now signed up!`)
-      .setDescription(
-        "If you need to remove your signup or modify it, please DM <@296070049062584321>"
-      );
+    await mainDatabase.registerSignUp({
+      discordID: interaction.user.id,
+      playerName: playerName,
+      position: position,
+      availability: availability,
+      info: info,
+    });
 
     const signUpEmbed = new MessageEmbed()
       .setColor("RANDOM")
@@ -100,20 +78,21 @@ module.exports = {
     if (info !== null && info.length > 1) {
       signUpEmbed.addField("Info", info, false);
     }
-    await updateSignUpList(bot);
+    await updateSignUpList(interaction.client);
 
-    const CHANNEL_ID = "894607908942278737";
-
-    await bot.channels.cache.get(CHANNEL_ID).send({
+    await interaction.client.channels.cache.get(REGISTERED_ID).send({
       embeds: [signUpEmbed],
     });
 
-    const FA_ROLE_ID = "892959397280620624";
-
-    await interaction.member.roles.add(FA_ROLE_ID);
+    await interaction.member.roles.add(FREE_AGENT_ROLE_ID);
 
     await interaction.editReply({
-      embeds: [successEmbed],
+      embeds: [
+        successEmbed(
+          "You are now signed up!",
+          "If you need to remove your signup or modify it, please DM <@296070049062584321>"
+        ),
+      ],
     });
   },
 };
