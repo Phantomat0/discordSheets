bd9437110a3284c632a5312408e09cfe8775f1e4
Phantomat0 <soldierxfreedom@gmail.com>
2021-10-26 14:20:35 -0500

    Added GUILD_MEMBERS Intent

--- discord/bot.js
+++ discord/bot.js
@@ -1,7 +1,11 @@
 const fs = require("fs");
 const { Client, Collection, Intents } = require("discord.js");
 const client = new Client({
-  intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MEMBERS,
+  ],
 });
 const { TOKEN } = require("./config/config");
 
