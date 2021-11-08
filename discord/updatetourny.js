1b0d9988f9468281c4b25418919423d470c024d3
Phantomat0 <soldierxfreedom@gmail.com>
2021-11-08 16:12:14 -0600

    Tournament embed

---
+++ discord/updatetourny.js
@@ -0,0 +1,35 @@
+const { MessageEmbed } = require("discord.js");
+const mainDatabase = require("../database/main/main");
+
+const getTournyPlayersEmbed = async () => {
+  const players = await mainDatabase.getPlayers();
+  const tournyPlayers = await mainDatabase.getTournyPlayers();
+
+  const playersString = tournyPlayers
+    .map((player, index) => {
+      const playerDiscordID = players.find(
+        (playerProfile) => playerProfile.player_id == player.player_id
+      ).discord_id;
+
+      return `${index + 1}. ${player.player_name} <@${playerDiscordID}>`;
+    })
+    .join("\n");
+
+  return new MessageEmbed()
+    .setTitle("6 Man Tournament Signups")
+    .setDescription(playersString);
+};
+
+const updateTournyPlayers = async (client) => {
+  const embed = await getTournyPlayersEmbed();
+
+  const TOURNY_SIGNUP_LIST_ID = "905133164744310864";
+  const TOURNY_SIGNUP_LIST_MESSAGE_ID = "905130017967726642";
+
+  client.channels.cache
+    .get(TOURNY_SIGNUP_LIST_ID)
+    .messages.fetch(TOURNY_SIGNUP_LIST_MESSAGE_ID)
+    .then((message) => message.edit({ embeds: [embed] }));
+};
+
+module.exports = updateTournyPlayers;
