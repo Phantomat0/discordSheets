fe2ba5b6ef33d15dbda725c0f31a80b2d4b807c4
Phantomat0 <soldierxfreedom@gmail.com>
2021-11-11 10:46:33 -0600

    onBan event

---
+++ discord/events/guildBanAdd.js
@@ -0,0 +1,17 @@
+const { MOD_LOG_ID } = require("../config/channels");
+
+module.exports = {
+  name: "guildBanAdd",
+  async execute(guild, user) {
+    console.log(user);
+    console.log(guild);
+
+    // Send message to Mod Log Channel
+    await guild.channels.cache.get(MOD_LOG_ID).send({
+      content: `BANNED LMAO PCE`,
+      //   embeds: [unmutedEmbed],
+    });
+
+    // client.login(TOKEN);
+  },
+};
