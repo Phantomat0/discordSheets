ee1bd429567d13610b3ae5adf184df82564eb05b
Phantomat0 <soldierxfreedom@gmail.com>
2021-11-17 11:03:10 -0600

    Ticket command

--- discord/commands/ticket.js
+++ discord/commands/ticket.js
@@ -1,6 +1,5 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
 const { MessageEmbed } = require("discord.js");
-const { threadId } = require("worker_threads");
 const { TICKETS_ID } = require("../config/channels");
 const { ADMIN_ROLE_ID } = require("../config/roles");
 
@@ -10,22 +9,46 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName("ticket")
     .setDescription("Submit a request ticket")
-    .addStringOption((option) =>
-      option
-        .setName("type")
-        .setDescription("Ticket type")
-        .addChoice("Discord Request", "discord-request")
-        .addChoice("Discord Report", "discord-report")
-        .addChoice("Pub Request", "pub-request")
-        .addChoice("Pub Ban Appeal", "pub-ban-appeal")
-        .setRequired(true)
-    )
-    .addStringOption((option) =>
-      option
-        .setName("description")
-        .setDescription("Shot description of the problem")
-        .setRequired(true)
+    .addSubcommand((subcommand) =>
+      subcommand
+        .setName("ban-appeal")
+        .setDescription("Ban Appeal")
+        .addStringOption((option) =>
+          option
+            .setRequired(true)
+            .setName("pub-name")
+            .setDescription("Name used when banned")
+        )
+        .addStringOption((option) =>
+          option
+            .setRequired(true)
+            .setName("room")
+            .setDescription("Room from which you were banned")
+            .addChoice("4v4", "4v4")
+            .addChoice("3v3", "3v3")
+            .addChoice("1v1", "1v1")
+            .addChoice("Multiple Rooms", "multiple")
+        )
+        .addStringOption((option) =>
+          option.setRequired(true).setName("reason").setDescription("Reason")
+        )
     ),
+  // .addStringOption((option) =>
+  //   option
+  //     .setName("type")
+  //     .setDescription("Ticket type")
+  //     .addChoice("Discord Request", "discord-request")
+  //     .addChoice("Discord Report", "discord-report")
+  //     .addChoice("Pub Request", "pub-request")
+  //     .addChoice("Pub Ban Appeal", "pub-ban-appeal")
+  //     .setRequired(true)
+  // )
+  // .addStringOption((option) =>
+  //   option
+  //     .setName("description")
+  //     .setDescription("Shot description of the problem")
+  //     .setRequired(true)
+  // ),
   async execute(interaction) {
     const ticketType = interaction.options.getString("type");
     const ticketDescription = interaction.options.getString("description");
