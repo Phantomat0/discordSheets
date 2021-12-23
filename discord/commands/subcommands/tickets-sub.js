c2cba8d53b54b3febfcf75f61de3a2a95f4c960e
Phantomat0 <soldierxfreedom@gmail.com>
2021-12-22 18:31:43 -0600

    Ban appeal command

---
+++ discord/commands/subcommands/tickets-sub.js
@@ -0,0 +1,296 @@
+const { MessageEmbed, MessageActionRow, MessageButton } = require("discord.js");
+const { TICKET_LOG_ID, MOD_LOG_ID } = require("../../config/channels");
+const { getDateTimeString, randomInRange } = require("../../utils/utils");
+
+const BAN_REASONS = {
+  trolling: "Trolling",
+  racial_slur: "Racial Slur",
+  harassment: "Harassment",
+  disruptive_behavior: "Disruptive Behavior",
+  strong_language: "Strong Language",
+  impersonation: "Impersonation",
+  blacklisted: "Blacklisted",
+  other: "Other",
+};
+
+class BanAppealManager {
+  constructor(
+    ticketInteraction,
+    buttonInteraction,
+    ticketLogMessage,
+    ticketObj,
+    randomInt
+  ) {
+    this.ticketInteraction = ticketInteraction;
+    this.buttonInteraction = buttonInteraction;
+    this.ticketLogMessage = ticketLogMessage;
+    this.ticketObj = ticketObj;
+    this.randomInt = randomInt;
+  }
+
+  decideButtonTypeAndHandle() {
+    const buttonID = this.buttonInteraction.customId;
+    if (buttonID === `thread${this.ticketInteraction.user.id}${this.randomInt}`)
+      return this._handleThread();
+    if (buttonID === `reject${this.ticketInteraction.user.id}${this.randomInt}`)
+      return this._handleReject();
+    if (buttonID === `accept${this.ticketInteraction.user.id}${this.randomInt}`)
+      return this._handleAccept();
+  }
+
+  async _sendAcceptMessage() {
+    const { playerNameOption, roomOption, banReason, appealOption } =
+      this.ticketObj;
+
+    const timeStamp = getDateTimeString();
+
+    const acceptEmbed = new MessageEmbed()
+      .setTitle("Ban Appeal Accepted")
+      .setColor("GREEN")
+      .setDescription(
+        `\`\`\`Name: ${playerNameOption}\nRoom: ${roomOption}\nReason: ${banReason}\nAppeal\n${appealOption}\n\`\`\`\n**By**: <@${this.buttonInteraction.user.id}>\n\n*${timeStamp}*`
+      );
+
+    await this.buttonInteraction.client.channels.cache.get(MOD_LOG_ID).send({
+      content: `<@${this.ticketInteraction.user.id}>`,
+      embeds: [acceptEmbed],
+    });
+  }
+
+  async _sendRejectMessage(rejectionReason) {
+    // If no reason is specified, make it say unspecified
+    rejectionReason =
+      rejectionReason === null ? "Unspecified" : rejectionReason;
+
+    const { playerNameOption, roomOption, banReason, appealOption } =
+      this.ticketObj;
+
+    const timeStamp = getDateTimeString();
+
+    const rejectionEmbed = new MessageEmbed()
+      .setTitle("Ban Appeal Denied")
+      .setColor("RED")
+      .setDescription(
+        `\`\`\`Name: ${playerNameOption}\nRoom: ${roomOption}\nReason: ${banReason}\nAppeal\n${appealOption}\n\`\`\`\n**Reason**: ${rejectionReason}\n**By**: <@${this.buttonInteraction.user.id}>\n\n*${timeStamp}*`
+      );
+
+    await this.buttonInteraction.client.channels.cache.get(MOD_LOG_ID).send({
+      content: `<@${this.ticketInteraction.user.id}>`,
+      embeds: [rejectionEmbed],
+    });
+  }
+
+  async _handleThread() {
+    this.buttonInteraction.reply({
+      content: "Thread created",
+      ephemeral: true,
+    });
+
+    const thread = await this.buttonInteraction.channel.threads.create({
+      name: "ban appeal",
+      type: "GUILD_PRIVATE_THREAD",
+      autoArchiveDuration: 1440,
+      reason: "Ban Appeal",
+    });
+
+    if (thread.joinable) await thread.join();
+
+    // Add the admin who clicked on the button to create thread
+    await thread.members.add(this.buttonInteraction.user.id);
+
+    // Add the player who created the ticket
+    await thread.members.add(this.ticketInteraction.user.id);
+
+    const deleteThreadButton = new MessageActionRow().addComponents([
+      new MessageButton()
+        .setStyle("DANGER")
+        .setLabel("Delete Thread")
+        .setCustomId("deleteThread"),
+    ]);
+
+    thread.send({
+      content: "Click the button below to delete this thread when complete",
+      components: [deleteThreadButton],
+    });
+
+    const deleteButtonCollector = thread.createMessageComponentCollector({
+      filter: (i) =>
+        i.user.id === this.buttonInteraction.user.id &&
+        i.customId === "deleteThread",
+      componentType: "BUTTON",
+      max: 1,
+    });
+
+    deleteButtonCollector.on("collect", async (i) => {
+      await thread.delete();
+    });
+  }
+
+  async _handleReject() {
+    this.buttonInteraction.reply({
+      content: 'Type a reason for this rejection below, or type "skip" to skip',
+      ephemeral: true,
+    });
+
+    const reasonCollector =
+      this.buttonInteraction.channel.createMessageCollector({
+        filter: (m) => m.author.id === this.buttonInteraction.user.id,
+        time: 15000,
+        max: 1,
+      });
+
+    reasonCollector.on("collect", async (m) => {
+      const message = m.content === "skip" ? null : m.content;
+      this._sendRejectMessage(message);
+      m.delete();
+      this.buttonInteraction.editReply("Command Complete");
+    });
+
+    reasonCollector.on("end", (collected) => {
+      if (collected.size >= 1) return;
+      this._sendRejectMessage(null);
+      this.buttonInteraction.editReply("Command Complete");
+    });
+
+    this.ticketLogMessage.delete();
+  }
+
+  async _handleAccept() {
+    this._sendAcceptMessage();
+    this.ticketLogMessage.delete();
+  }
+}
+
+async function appealCmd(interaction) {
+  const randomInt = randomInRange(1, 1000);
+
+  const { options } = interaction;
+
+  const playerNameOption = options.getString("pub-name");
+  const roomOption = options.getString("room");
+  const reasonOption = options.getString("reason");
+  const appealOption = options.getString("appeal");
+
+  const banReason = BAN_REASONS[reasonOption];
+
+  const ticketObj = {
+    playerNameOption,
+    roomOption,
+    banReason,
+    appealOption,
+  };
+
+  const appealEmbed = new MessageEmbed()
+    .setColor("#2F3136")
+    .setTitle("Ban Appeal")
+    .setDescription(
+      `**User**: <@${interaction.user.id}>\n**Name**: ${playerNameOption}\n**Room**: ${roomOption}\n**Reason**: ${banReason}\n**Appeal**\n${appealOption}`
+    );
+
+  const threadButton = new MessageButton()
+    .setCustomId(`thread${interaction.user.id}${randomInt}`)
+    .setStyle("SECONDARY")
+    .setLabel("Start Thread");
+  const rejectButton = new MessageButton()
+    .setCustomId(`reject${interaction.user.id}${randomInt}`)
+    .setStyle("DANGER")
+    .setLabel("Deny");
+  const acceptButton = new MessageButton()
+    .setCustomId(`accept${interaction.user.id}${randomInt}`)
+    .setStyle("SUCCESS")
+    .setLabel("Accept");
+
+  const buttonRow = new MessageActionRow().addComponents([
+    threadButton,
+    rejectButton,
+    acceptButton,
+  ]);
+
+  // Send the success ur thing was made!!!!!!
+  const ticketLogMessage = await interaction.client.channels.cache
+    .get(TICKET_LOG_ID)
+    .send({
+      embeds: [appealEmbed],
+      components: [buttonRow],
+    });
+
+  const buttonCollector =
+    ticketLogMessage.channel.createMessageComponentCollector({
+      filter: (i) => true,
+      componentType: "BUTTON",
+    });
+
+  buttonCollector.on("collect", async (i) => {
+    new BanAppealManager(
+      interaction,
+      i,
+      ticketLogMessage,
+      ticketObj,
+      randomInt
+    ).decideButtonTypeAndHandle();
+  });
+}
+
+async function nicknameCmd(interaction) {
+  const newDiscordNameOption = options.getString("nickname");
+
+  const nicknameEmbed = new MessageEmbed()
+    .setColor("#2F3136")
+    .setTitle("Nickname Change")
+    .setDescription(
+      `**User**: <@${interaction.user.id}>\n**Requested Name**: ${newDiscordNameOption}`
+    );
+
+  const rejectButton = new MessageButton()
+    .setCustomId(`rejectNick${interaction.user.id}`)
+    .setStyle("DANGER")
+    .setLabel("Reject");
+  const acceptButton = new MessageButton()
+    .setCustomId(`acceptNick${interaction.user.id}`)
+    .setStyle("SUCCESS")
+    .setLabel("Accept");
+
+  const buttonRow = new MessageActionRow().addComponents([
+    rejectButton,
+    acceptButton,
+  ]);
+
+  const ticketLogMessage = await interaction.client.channels.cache
+    .get(TICKET_LOG_ID)
+    .send({
+      embeds: [nicknameEmbed],
+      components: [buttonRow],
+    });
+
+  const buttonCollector =
+    ticketLogMessage.channel.createMessageComponentCollector({
+      filter: (i) => true,
+      componentType: "BUTTON",
+    });
+
+  buttonCollector.on("collect", async (i) => {
+    if (i.customId === `rejectNick${interaction.user.id}`) {
+    }
+
+    if (i.customId === `acceptNick${interaction.user.id}`) {
+      const timeStamp = getDateTimeString();
+
+      const acceptEmbed = new MessageEmbed()
+        .setTitle("Nickname Change Accepted")
+        .setColor("GREEN")
+        .setDescription(
+          `**User**: <@${interaction.user.id}>\n\n*${timeStamp}*`
+        );
+
+      await this.buttonInteraction.client.channels.cache.get(MOD_LOG_ID).send({
+        content: `<@${interaction.user.id}>`,
+        embeds: [acceptEmbed],
+      });
+    }
+  });
+}
+
+module.exports = new Map([
+  ["ban-appeal", appealCmd],
+  ["nickname", nicknameCmd],
+]);
--- discord/commands/ticket.js
+++ discord/commands/ticket.js
@@ -1,10 +1,10 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
-const { MessageEmbed } = require("discord.js");
-const { TICKETS_ID } = require("../config/channels");
-const { ADMIN_ROLE_ID } = require("../config/roles");
+const { TICKETS_ID, MOD_LOG_ID } = require("../config/channels");
+const { successEmbedCreator } = require("../utils/embeds");
+const subCommandsMap = require("./subcommands/tickets-sub");
 
 module.exports = {
-  allowedRoles: [ADMIN_ROLE_ID],
+  allowedRoles: [],
   allowedChannels: [TICKETS_ID],
   data: new SlashCommandBuilder()
     .setName("ticket")
@@ -27,10 +27,25 @@ module.exports = {
             .addChoice("4v4", "4v4")
             .addChoice("3v3", "3v3")
             .addChoice("1v1", "1v1")
+            .addChoice("Rambo", "rambo")
             .addChoice("Multiple Rooms", "multiple")
         )
         .addStringOption((option) =>
-          option.setRequired(true).setName("reason").setDescription("Reason")
+          option
+            .setRequired(true)
+            .setName("reason")
+            .setDescription("Reason")
+            .addChoice("Trolling", "trolling")
+            .addChoice("Racial Slur", "racial_slur")
+            .addChoice("Harassment", "harassment")
+            .addChoice("Disruptive Behavior", "disruptive_behavior")
+            .addChoice("Strong Language", "strong_language")
+            .addChoice("Impersonation", "impersonation")
+            .addChoice("Blacklisted", "blacklisted")
+            .addChoice("Other", "other")
+        )
+        .addStringOption((option) =>
+          option.setRequired(true).setName("appeal").setDescription("Appeal")
         )
     ),
   // .addStringOption((option) =>
@@ -50,60 +65,78 @@ module.exports = {
   //     .setRequired(true)
   // ),
   async execute(interaction) {
-    const ticketType = interaction.options.getString("type");
-    const ticketDescription = interaction.options.getString("description");
-    const ticketStarter = interaction.user;
+    const subCommandName = interaction.options.getSubcommand();
 
-    console.log(ticketStarter);
+    const command = subCommandsMap.get(subCommandName);
 
-    const thread = await interaction.channel.threads.create({
-      name: ticketType,
-      autoArchiveDuration: 1440,
-      reason: "CUZ",
-    });
+    if (!command)
+      throw new CommandError("Invalid Command", "That Command does not exist");
 
-    const ticketEmbed = new MessageEmbed()
-      .setTitle(ticketType)
-      .setDescription(
-        `By: ${ticketStarter.username}\nDescription: ${ticketDescription}\n\nFeel free to provide more details in regards to this ticket if necessary.`
-      );
+    await command(interaction);
 
-    thread.send({
-      embeds: [ticketEmbed],
-    });
-
-    if (thread.joinable) await thread.join();
-    await thread.members.add(interaction.user.id);
-
-    const addDiscordMods =
-      ticketType === "discord-request" || ticketType === "discord-report";
-    const addPubAdmins =
-      ticketType === "pub-ban-appeal" || ticketType === "pub-request";
-
-    const allMembers = await interaction.guild.members.fetch();
-
-    if (addDiscordMods) {
-      allMembers
-        .filter((player) =>
-          player.roles.cache.find((role) => role.name === "Moderator")
-        )
-        .forEach((user) => {
-          thread.members.add(user.user.id);
-        });
-    }
-
-    if (addPubAdmins) {
-      allMembers
-        .filter((player) =>
-          player.roles.cache.find((role) => role.name === "pub-admin")
-        )
-        .forEach((user) => {
-          thread.members.add(user.user.id);
-        });
-    }
+    const successEmbed = successEmbedCreator(
+      "Ticket submitted successfully!",
+      `You will be notified of a response to your ticket in <#${MOD_LOG_ID}>`
+    );
 
     interaction.editReply({
-      content: "Ticket created!",
+      embeds: [successEmbed],
     });
+
+    //   const ticketType = interaction.options.getString("type");
+    //   const ticketDescription = interaction.options.getString("description");
+    //   const ticketStarter = interaction.user;
+
+    //   console.log(ticketStarter);
+
+    //   const thread = await interaction.channel.threads.create({
+    //     name: ticketType,
+    //     autoArchiveDuration: 1440,
+    //     reason: "CUZ",
+    //   });
+
+    //   const ticketEmbed = new MessageEmbed()
+    //     .setTitle(ticketType)
+    //     .setDescription(
+    //       `By: ${ticketStarter.username}\nDescription: ${ticketDescription}\n\nFeel free to provide more details in regards to this ticket if necessary.`
+    //     );
+
+    //   thread.send({
+    //     embeds: [ticketEmbed],
+    //   });
+
+    //   if (thread.joinable) await thread.join();
+    //   await thread.members.add(interaction.user.id);
+
+    //   const addDiscordMods =
+    //     ticketType === "discord-request" || ticketType === "discord-report";
+    //   const addPubAdmins =
+    //     ticketType === "pub-ban-appeal" || ticketType === "pub-request";
+
+    //   const allMembers = await interaction.guild.members.fetch();
+
+    //   if (addDiscordMods) {
+    //     allMembers
+    //       .filter((player) =>
+    //         player.roles.cache.find((role) => role.name === "Moderator")
+    //       )
+    //       .forEach((user) => {
+    //         thread.members.add(user.user.id);
+    //       });
+    //   }
+
+    //   if (addPubAdmins) {
+    //     allMembers
+    //       .filter((player) =>
+    //         player.roles.cache.find((role) => role.name === "pub-admin")
+    //       )
+    //       .forEach((user) => {
+    //         thread.members.add(user.user.id);
+    //       });
+    //   }
+
+    //   interaction.editReply({
+    //     content: "Ticket created!",
+    //   });
   },
 };
